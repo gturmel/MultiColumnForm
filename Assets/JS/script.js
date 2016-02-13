bb776056e016f2354de1fccb1ed2f849b1a8d989
Greg Turmel <gregturmel@gmail.com>
2016-02-13 13:55:56 -0500

    add script

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -15,7 +15,7 @@ button.addEventListener("mouseenter", function(){
 button.addEventListener("mouseleave", function(){
    if (button.style.background = "#1478b1"){
    button.style.background = "#10689a"}
-}, false);
+});
 
 
 //this code will need to:
@@ -27,3 +27,48 @@ button.addEventListener("mouseleave", function(){
 // break this down.
 
 //button variable already declared (button)
+
+var form = document.querySelector("form");
+
+form.addEventListener("submit", function(){
+      console.log(form.firstName);
+      event.preventDefault();
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.middleName.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.lastName.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.Region.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.clans.value);
+      event.preventDefault();
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.games.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.cheese.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.movie.value);
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.pet.value);
+      event.preventDefault();
+});
+
+form.addEventListener("submit", function(){
+      console.log(form.petChoice.value);
+});
